68642f2ee6dad2445d0c3658ed825143a922642a
FOXFOXYS <foxfoxy.pro@gmail.com>
2020-07-28 13:23:24 +0200

    SECU + HIDE VERSION PHP

--- functions.php
+++ functions.php
@@ -4,3 +4,14 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+// retrait des erreurs de login de wordpress 
+add_filter('login_errors', 'wpm_hide_errors');
+
+function wpm_hide_errors() {
+    // On retourne notre propre phrase d'erreur
+    return "L'identifiant ou le mot de passe est incorrect";
+}
+
+// retait de la version php 
+remove_action('wp_head', 'wp_generator');
